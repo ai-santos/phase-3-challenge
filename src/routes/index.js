9499bbecb5cfb07d74a685220ec2bcb458234ad8
Aileen Santos <asantos3026@gmail.com>
2017-07-19 00:49:34 -0700

    Square route works

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,13 @@ router.get('/api/supported-operations', (request, response) => {
 	response.json({"/": "division","+": "addition", "-": "subtration", "*": "multiplication"})
 })
 
-router.get('/api/square?number', )
+router.get('/api/square', (request, response) => {
+	const number = request.query.number
+	const square = number * number
+	response.json({"result": square })
+})
+
+router.post('/api/compute')
 
 
 module.exports = router
